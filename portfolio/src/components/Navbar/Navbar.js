98de60f4fc16927a45cf3158658eca63d92e99d5
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-06-07 22:01:45 -0400

    add the base of navbar

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -2,9 +2,23 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-        
-    </div>
+    <nav>
+      <h1>Jeffrey Koshy</h1>
+      <ul>
+        <li>
+          <a href='#home' >Home</a>
+        </li>
+        <li>
+        <a href='#projects' >Projects</a>
+        </li>
+        <li>
+        <a href='#about' >About</a>
+        </li>
+        <li>
+        <a href='#contact' >Contact</a>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
