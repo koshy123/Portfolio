39a019fa0af07b5704d434c0f3c333da4e6c785e
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-06-15 17:42:47 -0400

    mobile hamburger is cleaned up

--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -1,25 +1,61 @@
-import React from 'react'
-import "./Navbar.css"
+import React, { useState } from 'react';
+import './Navbar.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 function Navbar() {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <nav className='nav'>
-      <h3>Jeffrey Koshy</h3>
-      <ul>
-        <li>
-          <a href='#home' >Home</a>
-        </li>
-        <li>
-        <a href='#projects' >Projects</a>
-        </li>
-        <li>
-        <a href='#about' >About</a>
-        </li>
-        <li>
-        <a href='#contact' >Contact</a>
-        </li>
-      </ul>
-    </nav>
-  )
+    <div>
+      <nav className='nav'>
+        <h3>Jeffrey Koshy</h3>
+        <ul>
+          <li>
+            <a href='#home'>Home</a>
+          </li>
+          <li>
+            <a href='#projects'>Projects</a>
+          </li>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a>
+          </li>
+          <li>
+            <i
+              className={`fa-solid fa-bars-staggered mobile-menu ${
+                isMobileMenuOpen ? 'open-menu' : 'closed-menu'
+              }`}
+              onClick={toggleMobileMenu}
+            ></i>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={`mobile-nav ${isMobileMenuOpen ? 'open-menu' : 'closed-menu'}`}>
+        <span className='mobile-menu' onClick={toggleMobileMenu}></span>
+        <ul>
+          <li>
+            <a href='#home'>Home</a>
+          </li>
+          <li>
+            <a href='#about'>About</a>
+          </li>
+          <li>
+            <a href='#projects'>Projects</a>
+          </li>
+          <li>
+            <a href='#contact'>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
