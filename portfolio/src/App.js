2c5e0fef75567fe3fbd92adb8e881f286901d16a
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-06-15 12:46:07 -0400

    add style to navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div >
-
+      <Navbar></Navbar>
     </div>
   );
 }
--- portfolio/src/components/Navbar/Navbar.js
+++ portfolio/src/components/Navbar/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./Navbar.css"
 function Navbar() {
   return (
-    <nav>
-      <h1>Jeffrey Koshy</h1>
+    <nav className='nav'>
+      <h3>Jeffrey Koshy</h3>
       <ul>
         <li>
           <a href='#home' >Home</a>
